5aeb6d016413b9d4ea019666c22f0bc225f79261
Xazu <Xazu001@users.noreply.github.com>
2023-04-26 16:00:25 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+let contactList = [
+    {
+        name: "Kacper Kijek",
+        phoneNumber: "506 952 548",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+    {
+        name: "Patrycja Kijek",
+        phoneNumber: "Nie pamiętam",
+    },
+];
+
+const renderContacts = () => {
+    const contactsContainer = document.querySelector("#contacts");
+    contactsContainer.innerHTML = "";
+
+    contactList.forEach((contact) => {
+        const newContact = `
+        <div class="contact">
+        <div class="contact-info"> 
+        <p>${contact.name}</p>
+        <span>${contact.phoneNumber}</span>
+        </div>
+        <div class="contact-remove">
+        <p>XXX</p>
+        </div>
+        </div>
+        `;
+
+        contactsContainer.innerHTML += newContact;
+
+    });
+};
+
+document.querySelector("#add-contact").addEventListener("click", () => {
+    const nameField = document.querySelector("#name");
+    const phoneNumberField = document.querySelector("#phone-number");
+
+    const newContact = {
+        name: nameField.value,
+        phoneNumber: phoneNumberField.value,
+    };
+
+    contactList.push(newContact);
+
+    renderContacts();
+
+    nameField.value = "";
+    phoneNumberField.value = "";
+
+})
+
+renderContacts();
+
